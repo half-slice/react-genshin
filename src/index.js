1742615c92ca9590f6518900ed43dc79d90b9c1c
half-slice <junbin2438@naver.com>
2022-07-06 21:47:32 +0900

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './component/App';
-import { app,analytics } from './fbase'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
